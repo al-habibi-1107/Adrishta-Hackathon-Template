2fa73dd05c610b3640088add5ba61915d40fbc8e
Kamil <kamilanwar2001@gmail.com>
2020-11-30 21:53:34 +0400

    admin page basic done

--- Application Code/app.js
+++ Application Code/app.js
@@ -7,24 +7,40 @@ const mongoose = require('mongoose');
 const app = express();
 
 // to serve up the static files 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // to set up body parser
 app.use(bodyParser.urlencoded({extended:true}));
 
+// to use templating for dynmic content
+app.set("view engine","ejs");
+
+
+var userQuestion ;
+var options=[];
+var totalUsers;
+var startTime;
+
 
 app.get('/admin',function(req,res){
-
-    res.sendFile(__dirname+"/admin.html");
+    res.render('admin',{pageName:"Admin Page"});
 });
 
 app.post('/admin',function(req,res){
 
-    console.log(req.body.ques);
-    console.log(req.body.op1);
-    console.log(req.body.op2);
-    console.log(req.body.op3);
-    console.log(req.body.totalVoters);
+    userQuestion = req.body.ques;
+    options.push(req.body.op1);
+    options.push(req.body.op2);
+    options.push(req.body.op3);
+    totalUsers = req.body.totalVoters;
+    if(userQuestion.length < 3||totalUsers<1){
+
+        res.render("adminRes",{err:true,pageName: "Admin Page"});
+    }
+    else {
+        res.render("adminRes",{err:false,pageName: "Admin Page"});
+    }
+        
 });
 
 
