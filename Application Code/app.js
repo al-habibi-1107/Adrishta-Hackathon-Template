6c54d3c41644b5088b3d366495e0d459128db84e
Kamil <kamilanwar2001@gmail.com>
2020-12-01 13:41:02 +0400

    Poll form done

--- Application Code/app.js
+++ Application Code/app.js
@@ -2,6 +2,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const readXlsxFile = require('read-excel-file/node');
+const multer = require('multer');
+const xlxs = require('xlsx');
+
+
 
 // setting up an instance of express
 const app = express();
@@ -16,9 +21,13 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 
 
-var userQuestion ;
-var options=[];
-var totalUsers;
+//to locate and save the xlxs file by the user
+
+
+var voteCounter;
+var userQuestion ="What has 4 legs?" ;
+var options=["man","dan","pan"];
+var totalUsers=5;
 var startTime;
 
 /////////////// ADMIN ROUTE /////////////////
@@ -28,11 +37,25 @@ app.get('/admin',function(req,res){
 
 app.post('/admin',function(req,res){
 
+    
+
+
     userQuestion = req.body.ques;
     options.push(req.body.op1);
     options.push(req.body.op2);
     options.push(req.body.op3);
     totalUsers = req.body.totalVoters;
+
+    
+    
+
+    //// to read and save the excel file to the server directory
+    // let filePath = __dirname + "/uploads/" + req.body.file;
+
+    //readXlsxFile(req.body.file).then(rows => console.log(rows));
+
+
+
     if(userQuestion.length < 3||totalUsers<1){
 
         res.render("adminRes",{err:true,pageName: "Admin Page"});
@@ -50,6 +73,10 @@ app.get('/polls',function(req,res){
     res.render('poll',{pageName:"Polls",question: userQuestion,op1: options[0],op2:options[1],op3:options[2]});
 });
 
+app.post('/polls',function(req,res){
+   console.log(req.body.mail);
+   console.log(req.body.op);
+});
 
 
 // set up the server to listen at a port
