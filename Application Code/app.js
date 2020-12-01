70e54a591cc6b1e887476c5b561b058f5b6fc3a1
Kamil <kamilanwar2001@gmail.com>
2020-12-01 19:44:44 +0400

    Logic done- Working Project

--- Application Code/app.js
+++ Application Code/app.js
@@ -1,10 +1,6 @@
 // initialising all packages
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const readXlsxFile = require('read-excel-file/node');
-const multer = require('multer');
-const xlxs = require('xlsx');
 const formidable = require('formidable');
 const fs = require('fs');
 
@@ -24,13 +20,12 @@ app.set("view engine","ejs");
 
 
 var voteCounter=0;
-var userQuestion ="What has 4 legs?" ;
+var userQuestion ="" ;
 var options=[];
-var optionsVote=[];
-var totalUsers=5;
-var startTime;
+var optionsVote=[0,0,0];
 var voterArr =[];
 var fileName;
+var flag =1;
 
 /////////////// ADMIN ROUTE /////////////////
 app.get('/admin',function(req,res){
@@ -53,18 +48,21 @@ app.post('/admin',function(req,res){
                         options.push(feilds.op2);
                         options.push(feilds.op3);
                         totalUsers = feilds.totalVoters;
+
+                        if(userQuestion.length<3){
+                    
+                            res.render("adminRes",{err:true,pageName: "Admin Page"});
+                        }
+                        else {
+                            res.render("adminRes",{err:false,pageName: "Admin Page"});
+                        }
+                        console.log(userQuestion);
+                        console.log(userQuestion.length);
     });
 
         
+   
 
-
-    if(userQuestion.length>3&&totalUsers<1){
-
-        res.render("adminRes",{err:true,pageName: "Admin Page"});
-    }
-    else {
-        res.render("adminRes",{err:false,pageName: "Admin Page"});
-    }
         
 });
 
@@ -77,10 +75,12 @@ app.get('/polls',function(req,res){
 
 app.post('/polls',function(req,res){
 
-
-  var array = fs.readFileSync(__dirname+'/uploads/'+fileName).toString().split("\n");
-   for(i in array) {
-       voterArr.push(array[i]);
+ if(flag==1){
+     var array = fs.readFileSync(__dirname+'/uploads/'+fileName).toString().split("\n");
+      for(i in array) {
+          voterArr.push(array[i]);
+          flag = 0;
+ }
    }
    if(voterArr.includes(req.body.mail)){
         if(req.body.op === options[0]){
@@ -94,7 +94,10 @@ app.post('/polls',function(req,res){
 
       var index = voterArr.indexOf(req.body.mail)
        console.log(index);
-        voterArr.splice(index,1);
+       if (index > -1) {
+        voterArr.splice(index, 1);
+      }
+        
    }
 
    console.log(voterArr);
@@ -104,6 +107,13 @@ app.post('/polls',function(req,res){
 
 });
 
+//////////////// RESULT ROUTE ///////////////////
+
+app.get('/result',function(req,res){
+
+    res.render('result',{pageName: "Result",op1:options[0],v1:optionsVote[0],op2:options[1],v2:optionsVote[1],op3:options[2],v3:optionsVote[2]});
+
+});
 
 // set up the server to listen at a port
 app.listen('3000',function(){
